26bfba12e0e296c6fbc675f8a7ba8ea250e210d6
divyanamdev06-cmd <divyanamdev06@gmail.com>
2026-05-06 22:41:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,18 @@
+// import mongoose from "mongoose";
+// export const connectDB= async()=>{
+//     try{
+//         // await mongoose.connect("mongodb://localhost:27017/12pmnew")
+//        await mongoose.connect("mongodb+srv://divyanamdev06_db_user:1neb4nVYYHUOMHMq@cluster0.fslughg.mongodb.net/12pmnew")
+//         console.log("database connected")
+//     }
+//     catch(error){
+//         console.log("database not connected")
+//     }
+//  }
 import mongoose from "mongoose";
 export const connectDB= async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/12pmnew")
+        await mongoose.connect("mongodb+srv://divyanamdev06_db_user:1neb4nVYYHUOMHMq@cluster0.fslughg.mongodb.net/12pmnew")
         console.log("database connected")
     }
     catch(error){
